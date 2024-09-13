e29aaa4f715c0efc5bdf696a9767594f80cb4d0c
Mayank kumar <mayankkumar.gehu821@gmail.com>
2024-09-13 17:58:34 +0530

    create headr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,28 @@
 import {FaSearch} from 'react-icons/fa';
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
   <header className='bg-blue-200 shadow-md'>
   <div className='flex justify-between items-center max-w-6xl mx-auto'>
-  <h1 className='font-bold font-sm sm:text-xl flex flex-wrap p-5'>
+  <Link to='/'>
+  <h1 className='font-bold font-sm sm:text-xl flex flex-wrap '>
   <span className='text-slate-500 '>Mayank</span>
   <span className='text-slate-700'>Estate</span>
-  </h1>
+  </h1></Link>
   <from className='bg-slate-100 p-3 rounded-lg flex items-center'>
     <input type="text" placeholder='search' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
     <FaSearch className='text-slate-600'/>
   </from>
   <ul className='flex gap-4'>
+<Link to='/'>
+    <li className='hidden sm:inline text-slate-700 hover:shadow-xl'>HOME</li></Link>
+<Link to='/about'>
+    <li className='hidden sm:inline text-slate-700 hover:shadow-xl'> ABOUT</li></Link>
 
-    <li className='hidden sm:inline text-slate-700 hover:shadow-xl'>HOME</li>
-
-    <li className='hidden sm:inline text-slate-700 hover:shadow-xl'> ABOUT</li>
-
-    <li className='hidden sm:inline text-slate-700 hover:shadow-xl'> SignIn</li>
-    <li className='hidden sm:inline text-slate-700 hover:shadow-xl'>SignOut</li>
+   <Link to='/Signin'> <li className=' text-slate-700 hover:shadow-xl'> SignIn</li> </Link>
+   <Link to='/Signout'> <li className=' text-slate-700 hover:shadow-xl'>SignOut</li> </Link>
   </ul>
   </div>
 </header>
